3e0d7f1e52a3dba980f12afdaf49a3ed5da2387e
hugo <hugo031c@gmail.com>
2026-04-09 19:54:00 +0200

    question 1.1 et 1.2

---
+++ src/scenario/Main.java
@@ -0,0 +1,16 @@
+package scenario;
+
+import produit.Poisson;
+import produit.Produit;
+import produit.Sanglier;
+import villageGaulois.Etal;
+import villageGaulois.IEtal;
+
+public class Main {
+	IEtal<Sanglier>[] marche = new IEtal[3];
+//	Etal[] marche = new Etal[3];
+	Etal<Sanglier> etalSanglier = new Etal<>();
+	Etal<Poisson> etalPoisson = new Etal<>();
+	marche[0] = etalSanglier;
+	marche[1] = etalPoisson;
+}
